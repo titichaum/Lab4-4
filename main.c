d37d610b79668117c851945b4184db207c7764f9
titichaum <69667820+titichaum@users.noreply.github.com>
2020-08-26 22:03:13 +0700

    Update main.c

--- main.c
+++ main.c
@@ -9,13 +9,12 @@ int main(void)
       scanf("%f",&L2);
       x=(L1/L2)*100;
       if(x>=40){x=(L2/L1)*100;}
-      if(x>= 40){//printf("%d %\n",x);
+      if(x>= 40){
       printf("Height :");
         scanf("%f",&H);
         Suml=L1+L2;
         Area=0.5*H*Suml;
         printf("Area= %.2f ",Area );
-        //printf("%d %\n",x);
       }else{printf("%d %\n",x);}
   return 0;
 }
